00c4dad81319396afffaaceb7b455f910ec5553d
Prashant-Kumar001 <prashant0kumar101@gmail.com>
2025-08-24 08:52:06 +0530

    Add MongoDB connection and type definitions

---
+++ lib/dB.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGO_URI!;
+
+if (!MONGODB_URI) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+let cachedDb = global.mongoose;
+
+if (!cachedDb) {
+    cachedDb = global.mongoose = { conn: null, promise: null };
+}
+
+export async function dbConnect() {
+    if (cachedDb.conn) {
+        return cachedDb.conn;
+    }
+
+    if (!cachedDb.promise) {
+        const opts = {
+            bufferCommands: false,
+            maxPoolSize: 10,
+        };
+        cachedDb.promise = mongoose.connect(MONGODB_URI, opts).then(() => mongoose.connection);
+    }
+    try {
+        cachedDb.conn = await cachedDb.promise;
+    } catch (error) {
+        cachedDb.promise = null;
+        throw error;
+    }
+
+}
+
+export default cachedDb;
